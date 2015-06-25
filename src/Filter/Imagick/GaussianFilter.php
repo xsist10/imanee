53bad3a5106d1d961044a9833adc592e76adb383
Tieme van Veen <tiemevanveen@hotmail.com>
2015-06-25 21:10:13 +0200

    Added Gaussian filter for Imagick

---
+++ src/Filter/Imagick/GaussianFilter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Imanee\Filter\Imagick;
+
+use Imanee\Imanee;
+use Imanee\Model\FilterInterface;
+
+class GaussianFilter implements FilterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(Imanee $imanee, array $options = [])
+    {
+        /** @var \Imagick $resource */
+        $resource = $imanee->getResource()->getResource();
+
+        $options = array_merge(['radius' => 2, 'sigma' => 2], $options);
+
+        return $resource->gaussianblurimage($options['radius'], $options['sigma']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'filter_gaussian';
+    }
+}
